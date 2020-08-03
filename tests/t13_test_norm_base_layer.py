a150f364dcae63ca52eaae6f829303c5eefd5ab4
mstettler <michael.stettler@cin.uni-tuebingen.de>
2020-08-03 13:35:11 +0200

    changed test name for norm base model

--- tests/t13_test_norm_base_layer.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-np.set_printoptions(precision=4, linewidth=150)
-
-print("Test Norm Base Layer")
-
-train_data = np.zeros(4)
-
---
+++ tests/t14_test_norm_base_layer.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+np.set_printoptions(precision=4, linewidth=150)
+"""
+run: python -m tests.t13_test_norm_base
+"""
+
+print("Test Norm Base Layer")
+
+n_features = 5
+n_frames = 1
+n_training_data = 1
+
+# create random test case
+np.random.seed = 0
+train_data = np.random.rand((n_features, n_frames, n_train_data))
+
