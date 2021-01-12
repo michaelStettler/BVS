ee58c58716dbbf6ccf781c526fd941436b7518c2
tim <ti.schaefer@student.uni-tuebingen.de>
2021-01-12 15:07:43 +0100

    improve projection plot

--- plot_directions.py
+++ plot_directions.py
@@ -1,6 +1,10 @@
 import os
 import numpy as np
+
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.legend_handler import HandlerPatch
+
 from utils.load_config import load_config
 from models.NormBase import NormBase
 from utils.load_data import load_data
@@ -36,17 +40,33 @@ projection, labels = norm_base.projection_tuning(data_test)
 x_lines, lines = norm_base.line_constant_activation()
 
 # plot
-fig, axs = plt.subplots(1, projection.shape[0], figsize=(5*projection.shape[0], 5))
-fig.suptitle("Projection of difference vector keeping 2-norm and scalar product")
-for category, ax in enumerate(axs):
-    ax.set_title('category {}'.format(category))
-    ax.scatter(projection[category,:,0], projection[category,:,1], s = 1, c=labels)
+fig, axs = plt.subplots(1, projection.shape[0]-1, figsize=(5*(projection.shape[0]-1), 5), sharey=True, sharex=True)
+fig.suptitle("Projection of difference vector preserving 2-norm and scalar product")
+for n_plot, ax in enumerate(axs):
+    category = n_plot +1
+    ax.set_title(["Neutral","Expression 1", "Expression 2"][category])
+    point_reference = ax.plot(0,0, 'kx', label="reference vector")
+    arrow_tuning = ax.arrow(0,0,1,0, label="tuning vector", color="black", head_width=0.05, length_includes_head=True)
+    #an_tuning = ax.annotate('', xy=(1, 0), xytext=(0, 0), arrowprops={'arrowstyle': '->'}, va='center')
+    scatter = ax.scatter(projection[category,:,0], projection[category,:,1], s = 1, c=labels)
 
     # set axis limits
-    ax.set_ylim(ymin=0, ymax=ax.get_ylim()[1]*1.2)
+    ax.set_ylim(ymin=-0.02, ymax=ax.get_ylim()[1]*1.2)
     x_max = max(max(np.abs(ax.get_xlim())), 1)
     ax.set_xlim(xmin=-x_max, xmax=x_max)
 
     #plot lines of constant activation
-    ax.plot(x_lines, lines, color="k", linewidth=0.5)
+    line_activation = ax.plot(x_lines, lines, color="k", linewidth=0.5)
+# legend
+handles_scatter, labels_scatter = scatter.legend_elements()
+def make_legend_arrow(legend, orig_handle,
+                      xdescent, ydescent,
+                      width, height, fontsize):
+    p = mpatches.FancyArrow(0, 0.5*height, width, 0, length_includes_head=True, head_width=0.75*height )
+    return p
+fig.legend([point_reference[0], arrow_tuning]+handles_scatter+[line_activation[0]],
+           ["reference vector", "tuning vector","Neutral","Expression 1","Expression 2","constant activation"],
+           handler_map={mpatches.FancyArrow : HandlerPatch(patch_func=make_legend_arrow),},
+           loc="upper right", borderaxespad=0.1)
+
 plt.savefig(os.path.join("models/saved", config['save_name'], save_name, "scatter.png"))
