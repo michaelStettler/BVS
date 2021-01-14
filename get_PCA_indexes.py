103fc2031715597b3638b5b14f3848a960c1c2d9
michaelStettler <michael.stettler.3@gmail.com>
2021-01-14 16:23:03 +0100

    started test PCA indexes

--- get_PCA_indexes.py
+++ get_PCA_indexes.py
@@ -1,6 +1,18 @@
 import numpy as np
+from sklearn.decomposition import PCA
 
-# set a random space
+# set a random seed
+np.random.seed(0)
 
 # built a fake dataset
+data = np.random.rand(4, 5)
+print("data")
+print(data)
 
+# apply normal PCA from scipy
+pca = PCA(n_components=2)
+x_new = pca.fit_transform(data)
+print("")
+print(x_new)
+
+# apply personal PCA
