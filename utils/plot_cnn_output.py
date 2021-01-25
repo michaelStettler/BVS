94ba2ad36d28ff0d6dc4103011351b772eb13df4
tim <ti.schaefer@student.uni-tuebingen.de>
2021-01-25 16:06:45 +0100

    add ability to highlight feature maps

--- utils/plot_cnn_output.py
+++ utils/plot_cnn_output.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 from matplotlib.animation import FuncAnimation
 import math
 import os
 
-def plot_cnn_output(output, path, name, title=None, image=None, video=False, verbose=True):
+def plot_cnn_output(output, path, name, title=None, image=None, video=False, verbose=True, highlight=None):
     """
     This function creates a plot of the cnn output.
     :param output: response from cnn (28,28,256)
@@ -15,6 +16,7 @@ def plot_cnn_output(output, path, name, title=None, image=None, video=False, ver
     :param video: if true an animation is created,
         output shape should then be (#frames, 28,28,256) and image either none or (#frames)
     :param verbose:
+    :param highlight: list of int, highlights the feature map at given indices by a red rectangle
     :return:
     """
     if verbose: print("start plot_cnn_output of", name)
@@ -32,6 +34,14 @@ def plot_cnn_output(output, path, name, title=None, image=None, video=False, ver
             image = image / 255
     if not os.path.exists(path):
         os.mkdir(path)
+    if not highlight is None:
+        for index_highlight in highlight:
+            multi_index_highlight = np.unravel_index(index_highlight, (n_rows,n_col))
+            if video: size_axis = output.shape[1]
+            else: size_axis = output.shape[0]
+            rec = Rectangle((0,0),size_axis,size_axis, fill=False, lw=size_axis*0.15, edgecolor='r')
+            rec = axs[multi_index_highlight].add_patch(rec)
+            rec.set_clip_on(False)
 
     update_list=[]
     def update(n_frame):
