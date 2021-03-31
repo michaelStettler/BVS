eba21022dd10e9ec85d6bb08cc4232bd9631035d
mstettler <michael.stettler@cin.uni-tuebingen.de>
2021-03-31 16:00:21 +0200

    added tests for Optical Flow

--- tests/CNN/t03_feature_map_positions.py
+++ tests/CNN/t03_feature_map_positions.py
@@ -34,38 +34,38 @@ model = tf.keras.Model(inputs=model.input, outputs=model.get_layer(config['v4_la
 size_ft = tuple(np.shape(model.output)[1:3])
 print("size_ft", size_ft)
 
-# ----------------------------------------------------------------------------------------------------------------------
-# test 1 - control weighted average
-# define feature map controls
-num_entry = 3
-num_ft = 1
-preds1 = np.zeros((num_entry,) + size_ft + (num_ft,))
-print("[TEST 1] shape predictions", np.shape(preds1))
-# -> should get (0, .33)
-preds1[1, 0, 0, 0] = 2
-preds1[1, 0, 1, 0] = 1
-
-# create test with 1 at each corners, should get -> (13,5, 13,5)
-preds1[2, 0, 0, 0] = 1
-preds1[2, 0, -1, 0] = 1
-preds1[2, -1, 0, 0] = 1
-preds1[2, -1, -1, 0] = 1
-
-# compute position vectors
-preds1_pos = calculate_position(preds1, mode="weighted average", return_mode="xy float")[..., 0]
-print("preds1_pos")
-print(preds1_pos)
-preds1_pos = calculate_position(preds1, mode="weighted average", return_mode="array")
-print("shape preds1_pos", np.shape(preds1_pos))
-
-# plot positions
-plot_cnn_output(preds1_pos, os.path.join("models/saved", config["config_name"]),
-                "test1_" + config['v4_layer'] + "_test1.gif",
-                # image="",
-                video=True,
-                verbose=False)
-print("[TEST 1] Finished plotting test1 positions")
-print()
+# # ----------------------------------------------------------------------------------------------------------------------
+# # test 1 - control weighted average
+# # define feature map controls
+# num_entry = 3
+# num_ft = 1
+# preds1 = np.zeros((num_entry,) + size_ft + (num_ft,))
+# print("[TEST 1] shape predictions", np.shape(preds1))
+# # -> should get (0, .33)
+# preds1[1, 0, 0, 0] = 2
+# preds1[1, 0, 1, 0] = 1
+#
+# # create test with 1 at each corners, should get -> (13,5, 13,5)
+# preds1[2, 0, 0, 0] = 1
+# preds1[2, 0, -1, 0] = 1
+# preds1[2, -1, 0, 0] = 1
+# preds1[2, -1, -1, 0] = 1
+#
+# # compute position vectors
+# preds1_pos = calculate_position(preds1, mode="weighted average", return_mode="xy float")[..., 0]
+# print("preds1_pos")
+# print(preds1_pos)
+# preds1_pos = calculate_position(preds1, mode="weighted average", return_mode="array")
+# print("shape preds1_pos", np.shape(preds1_pos))
+#
+# # plot positions
+# plot_cnn_output(preds1_pos, os.path.join("models/saved", config["config_name"]),
+#                 "test1_" + config['v4_layer'] + "_test1.gif",
+#                 # image="",
+#                 video=True,
+#                 verbose=False)
+# print("[TEST 1] Finished plotting test1 positions")
+# print()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # test 2 - control eye brow feature map
@@ -74,84 +74,84 @@ print("[TEST 2] Test semantic selection on morphing space")
 data = load_data(config)
 raw_seq = load_data(config, get_raw=True)[0]
 
-# predict
-preds = model.predict(data)[..., eyebrow_ft_idx]
-preds = np.expand_dims(preds, axis=3)
-preds = preds / np.amax(preds)  # normalize so we can compare with the positions
-print("[TEST 2] shape predictions", np.shape(preds))
-preds_init = preds[0]
-dyn_preds = preds - np.repeat(np.expand_dims(preds_init, axis=0), np.shape(preds)[0], axis=0)
-dyn_preds[dyn_preds < 0] = 0
-print("[TEST 2] shape dyn_preds", np.shape(dyn_preds))
+# # predict
+# preds = model.predict(data)[..., eyebrow_ft_idx]
+# preds = np.expand_dims(preds, axis=3)
+# preds = preds / np.amax(preds)  # normalize so we can compare with the positions
+# print("[TEST 2] shape predictions", np.shape(preds))
+# preds_init = preds[0]
+# dyn_preds = preds - np.repeat(np.expand_dims(preds_init, axis=0), np.shape(preds)[0], axis=0)
+# dyn_preds[dyn_preds < 0] = 0
+# print("[TEST 2] shape dyn_preds", np.shape(dyn_preds))
+#
+# # compute positions
+# preds_pos = calculate_position(preds, mode="weighted average", return_mode="array")
+# print("[TEST 2] shape preds_pos", np.shape(preds_pos))
+# dyn_preds_pos = calculate_position(dyn_preds, mode="weighted average", return_mode="array")
+# print("[TEST 2] shape dyn_preds_pos", np.shape(dyn_preds_pos))
+#
+# # concatenate prediction and position for plotting
+# results = np.concatenate((preds, preds_pos, dyn_preds, dyn_preds_pos), axis=3)
+# print("[TEST 2] shape results", np.shape(results))
+#
+# # plot results
+# plot_cnn_output(results, os.path.join("models/saved", config["config_name"]),
+#                 "test2_" + config['v4_layer'] + "_eye_brow_select_{}.gif".format(eyebrow_ft_idx),
+#                 image=raw_seq,
+#                 video=True,
+#                 verbose=False)
+# print("[TEST 2] Finished plotted cnn feature maps", np.shape(results))
+# print()
 
-# compute positions
-preds_pos = calculate_position(preds, mode="weighted average", return_mode="array")
-print("[TEST 2] shape preds_pos", np.shape(preds_pos))
-dyn_preds_pos = calculate_position(dyn_preds, mode="weighted average", return_mode="array")
-print("[TEST 2] shape dyn_preds_pos", np.shape(dyn_preds_pos))
-
-# concatenate prediction and position for plotting
-results = np.concatenate((preds, preds_pos, dyn_preds, dyn_preds_pos), axis=3)
-print("[TEST 2] shape results", np.shape(results))
-
-# plot results
-plot_cnn_output(results, os.path.join("models/saved", config["config_name"]),
-                "test2_" + config['v4_layer'] + "_eye_brow_select_{}.gif".format(eyebrow_ft_idx),
-                image=raw_seq,
-                video=True,
-                verbose=False)
-print("[TEST 2] Finished plotted cnn feature maps", np.shape(results))
-print()
-
-# ----------------------------------------------------------------------------------------------------------------------
-# test 3 - test average of floating points
-# compare between 1 feature map to all feature maps
-# predict responses for all eyebrow units
-print("[TEST 3] Get all eye brow semantic units")
-eyebrow_ft_idx = [148, 209, 208, 67, 211, 141, 90, 196, 174, 179, 59, 101, 225, 124, 125, 156]
-preds = model.predict(data)[..., eyebrow_ft_idx]
-preds = preds / np.amax(preds)  # normalize so we can compare with the positions
-print("[TEST 3] shape predictions", np.shape(preds))
-preds_init = preds[0]
-dyn_preds = preds - np.repeat(np.expand_dims(preds_init, axis=0), np.shape(preds)[0], axis=0)
-dyn_preds[dyn_preds < 0] = 0
-
-# compute floating value positions
-dyn_pos = calculate_position(dyn_preds, mode="weighted average", return_mode="xy float")
-print("[TEST 3] Shape dyn_pos", np.shape(dyn_pos))
-dyn_pos_mean = np.mean(dyn_pos, axis=2)
-print("[TEST 3] Shape dyn_pos_mean", np.shape(dyn_pos_mean))
-
-# set color to represent time
-color_seq = np.arange(len(dyn_pos_mean))
-# plot raw positions of first feature map
-plt.figure()
-plt.scatter(dyn_pos[:, 1, 0], dyn_pos[:, 0, 0], c=color_seq)  # plot first feature map
-plt.colorbar()
-plt.savefig(os.path.join("models/saved", config["config_name"], "test3_ft0_dyn_xy_float_pos"))
-
-# plot mean positions over all eyebrow feature map
-plt.figure()
-plt.scatter(dyn_pos_mean[:, 1], dyn_pos_mean[:, 0], c=color_seq)  # plot first feature map
-plt.colorbar()
-plt.savefig(os.path.join("models/saved", config["config_name"], "test3_mean_dyn_xy_float_pos"))
-
-# create array from float value to plot with cnn output function
-size_mean_ft = 56
-mean_feature_map = np.zeros((len(dyn_pos_mean), size_mean_ft, size_mean_ft, 1))
-for i in range(len(dyn_pos_mean)):
-    x = int(np.round(dyn_pos_mean[i, 0] * (size_mean_ft/28)))
-    y = int(np.round(dyn_pos_mean[i, 1] * (size_mean_ft/28)))
-    mean_feature_map[i, x, y, 0] = 1
-
-# plot mean positions
-plot_cnn_output(mean_feature_map, os.path.join("models/saved", config["config_name"]),
-                "test3_" + config['v4_layer'] + "_mean_xy_float_pos.gif",
-                image=raw_seq,
-                video=True,
-                verbose=False)
-print("[Test 3] Finish plotting feature maps")
-print()
+# # ----------------------------------------------------------------------------------------------------------------------
+# # test 3 - test average of floating points
+# # compare between 1 feature map to all feature maps
+# # predict responses for all eyebrow units
+# print("[TEST 3] Get all eye brow semantic units")
+# eyebrow_ft_idx = [148, 209, 208, 67, 211, 141, 90, 196, 174, 179, 59, 101, 225, 124, 125, 156]
+# preds = model.predict(data)[..., eyebrow_ft_idx]
+# preds = preds / np.amax(preds)  # normalize so we can compare with the positions
+# print("[TEST 3] shape predictions", np.shape(preds))
+# preds_init = preds[0]
+# dyn_preds = preds - np.repeat(np.expand_dims(preds_init, axis=0), np.shape(preds)[0], axis=0)
+# dyn_preds[dyn_preds < 0] = 0
+#
+# # compute floating value positions
+# dyn_pos = calculate_position(dyn_preds, mode="weighted average", return_mode="xy float")
+# print("[TEST 3] Shape dyn_pos", np.shape(dyn_pos))
+# dyn_pos_mean = np.mean(dyn_pos, axis=2)
+# print("[TEST 3] Shape dyn_pos_mean", np.shape(dyn_pos_mean))
+#
+# # set color to represent time
+# color_seq = np.arange(len(dyn_pos_mean))
+# # plot raw positions of first feature map
+# plt.figure()
+# plt.scatter(dyn_pos[:, 1, 0], dyn_pos[:, 0, 0], c=color_seq)  # plot first feature map
+# plt.colorbar()
+# plt.savefig(os.path.join("models/saved", config["config_name"], "test3_ft0_dyn_xy_float_pos"))
+#
+# # plot mean positions over all eyebrow feature map
+# plt.figure()
+# plt.scatter(dyn_pos_mean[:, 1], dyn_pos_mean[:, 0], c=color_seq)  # plot first feature map
+# plt.colorbar()
+# plt.savefig(os.path.join("models/saved", config["config_name"], "test3_mean_dyn_xy_float_pos"))
+#
+# # create array from float value to plot with cnn output function
+# size_mean_ft = 56
+# mean_feature_map = np.zeros((len(dyn_pos_mean), size_mean_ft, size_mean_ft, 1))
+# for i in range(len(dyn_pos_mean)):
+#     x = int(np.round(dyn_pos_mean[i, 0] * (size_mean_ft/28)))
+#     y = int(np.round(dyn_pos_mean[i, 1] * (size_mean_ft/28)))
+#     mean_feature_map[i, x, y, 0] = 1
+#
+# # plot mean positions
+# plot_cnn_output(mean_feature_map, os.path.join("models/saved", config["config_name"]),
+#                 "test3_" + config['v4_layer'] + "_mean_xy_float_pos.gif",
+#                 image=raw_seq,
+#                 video=True,
+#                 verbose=False)
+# print("[Test 3] Finish plotting feature maps")
+# print()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # test 4 - test mean feature maps
@@ -163,14 +163,19 @@ eye_brow_mean_pred = np.mean(preds, axis=3)
 eye_brow_mean_pred = eye_brow_mean_pred / np.amax(eye_brow_mean_pred)  # normalize so we can compare with the positions
 eye_brow_mean_pred = np.expand_dims(eye_brow_mean_pred, axis=3)
 print("[TEST 4] Shape eye_brow_mean_pred", np.shape(eye_brow_mean_pred))
+
+# compute dynamic
 eye_brow_mean_pred_init = np.repeat(np.expand_dims(eye_brow_mean_pred[0], axis=0), len(eye_brow_mean_pred), axis=0)
 dyn_eye_brow_mean_pred = eye_brow_mean_pred - eye_brow_mean_pred_init
 dyn_eye_brow_mean_pred[dyn_eye_brow_mean_pred < 0] = 0
-# dyn_eye_brow_mean_pred = np.exp(dyn_eye_brow_mean_pred)
+dyn_eye_brow_mean_pred = np.exp(2*dyn_eye_brow_mean_pred) - 1
 print("min max dyn_eye_brow_mean_pred", np.amin(dyn_eye_brow_mean_pred), np.amax(dyn_eye_brow_mean_pred))
-dyn_eye_brow_mean_pred = dyn_eye_brow_mean_pred - np.amin(dyn_eye_brow_mean_pred)
-dyn_eye_brow_mean_pred = dyn_eye_brow_mean_pred / np.amax(dyn_eye_brow_mean_pred)  # normalize so we can compare with the positions
+# dyn_eye_brow_mean_pred = dyn_eye_brow_mean_pred - np.amin(dyn_eye_brow_mean_pred)
+# dyn_eye_brow_mean_pred = dyn_eye_brow_mean_pred / np.amax(dyn_eye_brow_mean_pred)  # normalize so we can compare with the positions
 print("[TEST 4] shape dyn_preds", np.shape(dyn_eye_brow_mean_pred))
+
+# set color to represent time
+color_seq = np.arange(len(dyn_eye_brow_mean_pred))
 
 # print raw response
 plt.figure()
@@ -220,60 +225,60 @@ plot_cnn_output(results, os.path.join("models/saved", config["config_name"]),
 print("[Test 4] Finish plotting results")
 print()
 
-# ----------------------------------------------------------------------------------------------------------------------
-# test 5 - test mean feature maps on lips
-print("[TEST 5] Test on lips units")
-lips_ft_idx = [79, 120, 125, 0, 174, 201, 193, 247, 77, 249, 210, 149, 89, 197, 9, 251, 237, 165, 101, 90, 27, 158, 154,
-               10, 168, 156, 44, 23, 34, 85, 207]
-
-# load c3 expression to test mouth movements
-config["train_expression"] = ["c3"]
-data = load_data(config)
-raw_seq = load_data(config, get_raw=True)[0]
-print("[TEST 5] Loaded c3 expression")
-
-# predict data and keep only the lips units
-preds = model.predict(data)[..., lips_ft_idx]
-# preds = preds / np.amax(preds)  # normalize so we can compare with the positions
-print("[TEST 5] shape predictions", np.shape(preds))
-
-# compute mean directly on the feature maps
-lips_mean_pred = np.mean(preds, axis=3)
-lips_mean_pred = lips_mean_pred / np.amax(lips_mean_pred)  # normalize so we can compare with the positions
-lips_mean_pred = np.expand_dims(lips_mean_pred, axis=3)
-print("[TEST 5] Shape lips_mean_pred", np.shape(lips_mean_pred))
-lips_mean_pred_init = np.repeat(np.expand_dims(lips_mean_pred[0], axis=0), len(lips_mean_pred), axis=0)
-dyn_lips_mean_pred = lips_mean_pred - lips_mean_pred_init
-dyn_lips_mean_pred[dyn_lips_mean_pred < 0] = 0
-print("[TEST 5] shape dyn_preds", np.shape(dyn_lips_mean_pred))
-
-# plot raw positions of first feature map
-preds_pos = calculate_position(lips_mean_pred, mode="weighted average", return_mode="xy float")
-plt.figure()
-plt.scatter(preds_pos[:, 1, 0], preds_pos[:, 0, 0], c=color_seq)  # plot first feature map
-plt.colorbar()
-plt.savefig(os.path.join("models/saved", config["config_name"], "test5_lips_mean_xy_float_pos"))
-
-# plot mean positions over all eyebrow feature map
-dyn_preds_pos = calculate_position(dyn_lips_mean_pred, mode="weighted average", return_mode="xy float")
-plt.figure()
-plt.scatter(dyn_preds_pos[:, 1, 0], dyn_preds_pos[:, 0, 0], c=color_seq)  # plot first feature map
-plt.colorbar()
-plt.savefig(os.path.join("models/saved", config["config_name"], "test5_lips_mean_dyn_xy_float_pos"))
-
-# compute positions
-preds_pos = calculate_position(lips_mean_pred, mode="weighted average", return_mode="array")
-print("[TEST 5] shape preds_pos", np.shape(preds_pos))
-dyn_preds_pos = calculate_position(dyn_lips_mean_pred, mode="weighted average", return_mode="array")
-print("[TEST 5] shape dyn_preds_pos", np.shape(dyn_preds_pos))
-
-# concatenate prediction and position for plotting
-results = np.concatenate((lips_mean_pred, preds_pos, dyn_lips_mean_pred, dyn_preds_pos), axis=3)
-print("[TEST 5] shape results", np.shape(results))
-
-# plot means feature maps and positions
-plot_cnn_output(results, os.path.join("models/saved", config["config_name"]),
-                "test5_" + config['v4_layer'] + "_lips_mean_feature_map.gif",
-                image=raw_seq,
-                video=True,
-                verbose=False)
+# # ----------------------------------------------------------------------------------------------------------------------
+# # test 5 - test mean feature maps on lips
+# print("[TEST 5] Test on lips units")
+# lips_ft_idx = [79, 120, 125, 0, 174, 201, 193, 247, 77, 249, 210, 149, 89, 197, 9, 251, 237, 165, 101, 90, 27, 158, 154,
+#                10, 168, 156, 44, 23, 34, 85, 207]
+#
+# # load c3 expression to test mouth movements
+# config["train_expression"] = ["c3"]
+# data = load_data(config)
+# raw_seq = load_data(config, get_raw=True)[0]
+# print("[TEST 5] Loaded c3 expression")
+#
+# # predict data and keep only the lips units
+# preds = model.predict(data)[..., lips_ft_idx]
+# # preds = preds / np.amax(preds)  # normalize so we can compare with the positions
+# print("[TEST 5] shape predictions", np.shape(preds))
+#
+# # compute mean directly on the feature maps
+# lips_mean_pred = np.mean(preds, axis=3)
+# lips_mean_pred = lips_mean_pred / np.amax(lips_mean_pred)  # normalize so we can compare with the positions
+# lips_mean_pred = np.expand_dims(lips_mean_pred, axis=3)
+# print("[TEST 5] Shape lips_mean_pred", np.shape(lips_mean_pred))
+# lips_mean_pred_init = np.repeat(np.expand_dims(lips_mean_pred[0], axis=0), len(lips_mean_pred), axis=0)
+# dyn_lips_mean_pred = lips_mean_pred - lips_mean_pred_init
+# dyn_lips_mean_pred[dyn_lips_mean_pred < 0] = 0
+# print("[TEST 5] shape dyn_preds", np.shape(dyn_lips_mean_pred))
+#
+# # plot raw positions of first feature map
+# preds_pos = calculate_position(lips_mean_pred, mode="weighted average", return_mode="xy float")
+# plt.figure()
+# plt.scatter(preds_pos[:, 1, 0], preds_pos[:, 0, 0], c=color_seq)  # plot first feature map
+# plt.colorbar()
+# plt.savefig(os.path.join("models/saved", config["config_name"], "test5_lips_mean_xy_float_pos"))
+#
+# # plot mean positions over all eyebrow feature map
+# dyn_preds_pos = calculate_position(dyn_lips_mean_pred, mode="weighted average", return_mode="xy float")
+# plt.figure()
+# plt.scatter(dyn_preds_pos[:, 1, 0], dyn_preds_pos[:, 0, 0], c=color_seq)  # plot first feature map
+# plt.colorbar()
+# plt.savefig(os.path.join("models/saved", config["config_name"], "test5_lips_mean_dyn_xy_float_pos"))
+#
+# # compute positions
+# preds_pos = calculate_position(lips_mean_pred, mode="weighted average", return_mode="array")
+# print("[TEST 5] shape preds_pos", np.shape(preds_pos))
+# dyn_preds_pos = calculate_position(dyn_lips_mean_pred, mode="weighted average", return_mode="array")
+# print("[TEST 5] shape dyn_preds_pos", np.shape(dyn_preds_pos))
+#
+# # concatenate prediction and position for plotting
+# results = np.concatenate((lips_mean_pred, preds_pos, dyn_lips_mean_pred, dyn_preds_pos), axis=3)
+# print("[TEST 5] shape results", np.shape(results))
+#
+# # plot means feature maps and positions
+# plot_cnn_output(results, os.path.join("models/saved", config["config_name"]),
+#                 "test5_" + config['v4_layer'] + "_lips_mean_feature_map.gif",
+#                 image=raw_seq,
+#                 video=True,
+#                 verbose=False)
--- tests/CNN/t04_optical_flow_on_ft.py
+++ tests/CNN/t04_optical_flow_on_ft.py
@@ -36,6 +36,7 @@ eyebrow_ft_idx = 148   # the index comes from t02_find_semantic_units, it is the
 model = load_extraction_model(config, input_shape=tuple(config["input_shape"]))
 model = tf.keras.Model(inputs=model.input, outputs=model.get_layer(config['v4_layer']).output)
 size_ft = tuple(np.shape(model.output)[1:3])
+size_ft = (280, 280)
 print("size_ft", size_ft)
 
 # load morphing sequence
@@ -57,6 +58,12 @@ print()
 # transform to gray images
 predictions = preds[..., 0]
 
+# plot first feature map
+pred0 = np.array(predictions[0]) * 255
+print("shape pred0", np.shape(pred0))
+pred0_fm148 = cv2.cvtColor(pred0.astype(np.uint8), cv2.COLOR_GRAY2BGR)
+cv2.imwrite(os.path.join(path, 'pred0_fm148.png'), pred0_fm148)
+
 # Creates an image filled with zero intensities with the same dimensions as the frame
 mask = np.zeros(size_ft + (3, ))  # add tuple to set up size of rgb image
 print("[TEST 1] shape mask", np.shape(mask))
@@ -66,25 +73,86 @@ mask[..., 1] = 255
 # initialize video writer
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 fps = 30
-video_filename = os.path.join(path, 'output.mp4')
-out = cv2.VideoWriter(video_filename, fourcc, fps, size_ft)
+of_video_filename = os.path.join(path, 'optical_flow_fm148.mp4')
+of_out = cv2.VideoWriter(of_video_filename, fourcc, fps, size_ft)
+# create raw output
+fm_video_filename = os.path.join(path, 'raw_output_fm148.mp4')
+fm_out = cv2.VideoWriter(fm_video_filename, fourcc, fps, size_ft)
 
-for i in range(1, np.shape(predictions)[0]):
-    # compute optical flow  todo check what are all those parameters...
-    flow = cv2.calcOpticalFlowFarneback(predictions[i - 1], predictions[i], None, 0.5, 3, 15, 3, 5, 1.2, 0)
+# for i in range(1, np.shape(predictions)[0]):
+for i in range(1, 5):
+    # get current and previous frame
+    prev_frame = np.array(np.array(predictions[i - 1]) * 255).astype(np.uint8)
+    curr_frame = np.array(np.array(predictions[i]) * 255).astype(np.uint8)
+
+    # tests
+    # curr_frame = np.zeros(size_ft).astype(np.uint8)
+    prev_frame = np.zeros(size_ft).astype(np.uint8)
+    curr_frame = np.zeros(size_ft).astype(np.uint8)
+    # single dot
+    # prev_frame[100:101, (30+i):(31+i)] = 255  # for size_ft (280, 280)
+    # curr_frame[100:101, (31+i):(32+i)] = 255
+    # cube
+    prev_frame[100:110, (30+i):(40+i)] = 255  # for size_ft (280, 280)
+    curr_frame[100:110, (31+i):(41+i)] = 255
+    # single dot
+    # prev_frame[10:11, (3+i):(4+i)] = 255  # for size_ft (28, 28)
+    # curr_frame[10:11, (4+i):(5+i)] = 255
+    # cube
+    # prev_frame[10:14, (3+i):(7+i)] = 255  # for size_ft (28, 28)
+    # curr_frame[10:14, (4+i):(8+i)] = 255
+    print("shape curr_frame", np.shape(curr_frame))
+    print("min max curr_frame", np.amin(curr_frame), np.amax(curr_frame))
+
+    # transform current frame to BGR for visualization
+    fm = cv2.cvtColor(curr_frame, cv2.COLOR_GRAY2BGR)
+
+    # compute optical flow
+    # parameters explanation: https://www.geeksforgeeks.org/opencv-the-gunnar-farneback-optical-flow/
+    #   - winsize: It is the average window size, larger the size, the more robust the algorithm is to noise, and
+    #   provide fast motion detection, though gives blurred motion fields.
+    #   - poly_n : It is typically 5 or 7, it is the size of the pixel neighbourhood which is used to find polynomial
+    #   expansion between the pixels.
+    flow = cv2.calcOpticalFlowFarneback(prev_frame, curr_frame,
+                                        flow=None,
+                                        pyr_scale=0.5,
+                                        levels=1,
+                                        winsize=3,  # 15
+                                        iterations=5,  # 3
+                                        poly_n=5,  # 5
+                                        poly_sigma=1.2,
+                                        flags=0)
 
     # build image
     # Computes the magnitude and angle of the 2D vectors
     magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    print("shape magnitude", np.shape(magnitude))
+    print("min max magnitude", np.amin(magnitude), np.amax(magnitude))
+    print("shape angle", np.shape(angle))
+    print("min max angle", np.amin(angle), np.amax(angle))
     # Sets image hue according to the optical flow direction
     mask[..., 0] = angle * 180 / np.pi / 2
     # Sets image value according to the optical flow magnitude (normalized)
     mask[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
     # Converts HSV to RGB (BGR) color representation
+    print("min max mask[..., 0]", np.amin(mask[..., 0]), np.amax(mask[..., 0]))
+    print("min max mask[..., 1]", np.amin(mask[..., 1]), np.amax(mask[..., 1]))
+    print("min max mask[..., 2]", np.amin( mask[..., 2]), np.amax( mask[..., 2]))
     bgr = cv2.cvtColor(mask.astype('float32'), cv2.COLOR_HSV2BGR)
+    # bgr = np.array(bgr).astype(np.uint8)
+    # bgr[bgr < 0] = 0
+    # bgr[:5, :5, 0] = 255
+    print("min max bgr", np.amin(bgr), np.amax(bgr))
+    print("min max b channel", np.amin(bgr[..., 0]), np.amax(bgr[..., 0]))
+    print("min max g channel", np.amin(bgr[..., 1]), np.amax(bgr[..., 1]))
+    print("min max r channel", np.amin(bgr[..., 2]), np.amax(bgr[..., 2]))
 
+    cv2.imwrite(os.path.join(path, "bgr_{}.png".format(i)), bgr)
     # write image
-    out.write(bgr)
+    of_out.write(bgr)  # seems to create weird horizontal lines
+    fm_out.write(fm)
+    print()
 
-out.release()
+of_out.release()
+fm_out.release()
 print("[TEST 1] Finish optical flow on first feature map")
