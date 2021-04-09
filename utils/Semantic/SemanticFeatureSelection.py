3f72a4f70da1b623de6e730097f7a11b4fe1d6fd
Michael Stettler <michael.stettler@cin.uni-tuebingen.de>
2021-04-09 18:49:58 +0200

    started transform function of the semantic feature selection

--- utils/Semantic/SemanticFeatureSelection.py
+++ utils/Semantic/SemanticFeatureSelection.py
@@ -6,6 +6,7 @@ from utils.Semantic.load_coco_semantic_annotations import load_coco_categories
 from utils.Semantic.load_coco_semantic_annotations import get_coco_cat_ids
 from utils.Semantic.find_semantic_units import find_semantic_units
 from utils.Semantic.find_semantic_units import get_IoU_per_category
+from utils.feat_map_filter_processing import get_feat_map_filt_preds
 
 
 class SemanticFeatureSelection:
@@ -32,11 +33,30 @@ class SemanticFeatureSelection:
         print("[FIT] Start computing semantic units")
         self.sem_idx_list = find_semantic_units(model, data, self.config)
 
-    def transform(self, data):
-        print("TODO WOULOUHOUOUOUOUOU")
+    def transform(self, preds):
         if self.sem_idx_list is None:
             raise ValueError("Semantic features units are None! Please either train the Semantic Feature Selection or "
                              "load a pre-trained dictionary")
+        else:
+            print("TODO WOULOUHOUOUOUOUOU")
+
+            # get category IDs of interest
+            cat_ids = get_coco_cat_ids(config, cat_of_interest, to_numpy=True)
+
+            # translate categories IDs to CNN feature map indexes
+            cat_feature_map_indexes = get_IoU_per_category(sem_idx_list, cat_ids)  # todo save this in fit?
+
+            # transform predictions
+            # todo add all parameters to config!
+            preds = get_feat_map_filt_preds(preds,
+                                                  ft_idx,
+                                                  ref_type="self0",
+                                                  norm=1000,
+                                                  activation='ReLu',
+                                                  filter='spatial_mean',
+                                                  verbose=True)
+
+        return preds
 
     # def save(self, path):
     #     with open(os.path.join(path, 'semantic_dictionary.pkl'), 'wb') as f:
