7f40791f505ca77cea1c2bd554d65cc59652b0a6
Michael Stettler <michael.stettler.3@gmail.com>
2022-12-22 10:18:55 +0100

    added sweep config to train CNN

--- projects/behavourial/03_CNN_training.py
+++ projects/behavourial/03_CNN_training.py
@@ -1,7 +1,9 @@
 import os
+os.environ["WANDB_SILENT"] = "true"
 import numpy as np
 import datetime
 import tensorflow as tf
+import pprint
 
 from utils.load_config import load_config
 from utils.load_data import load_data
@@ -23,7 +25,24 @@ config = load_config(config_path, path='configs/behavourial')
 import wandb
 from wandb.keras import WandbCallback
 from wandb.keras import WandbMetricsLogger
-run = wandb.init(project=config["project"], entity="bvs")
+
+#%% set sweep config
+sweep_config = {
+    'method': 'grid',
+    'metric': {
+        'name': 'val_loss',  # val_accuracy?
+        'goal': 'minimize'  # maximize?
+    },
+    'parameters': {
+        'lr': {'values': [0.0001, 0.00001]},
+        'epoch': {'value': 40}
+    }
+
+}
+pprint.pprint(sweep_config)
+
+# create sweep id
+sweep_id = wandb.sweep(sweep_config, config["project"])
 
 #%% import data
 train_data = load_data(config, get_raw=True)
@@ -41,72 +60,63 @@ print("n_val", n_val)
 print("n_steps", n_steps)
 print()
 
-#%% set class weight
-class_weight = {}
-for i in range(config["n_category"]):
-    n_data = len(train_data[1][train_data[1] == i])
-    class_weight[i] = (1/n_data) * (len(train_data[1]) / 4)
-    print("num data {}: {}".format(i, n_data))
-# class_weight = {0: 0.5, 1: 1.0, 2: 1.0, 3: 1.0, 4: 1.0}
-class_weight = None
-print("class weights:", class_weight)
 
-#%% create wandb config
-wandb.config = {
-    "learning_rate": config["base_learning_rate"],
-    "epochs": config["initial_epochs"],
-    "momentum": config["momentum"],
-    "batch_size": config["batch_size"]
-}
+def train(config):
+    run = wandb.init(config=config)
+    with run:
+        config = wandb.config
 
-#%% create model
-# declare layers
-data_augmentation = tf.keras.Sequential([
-  tf.keras.layers.RandomFlip('horizontal'),
-  tf.keras.layers.RandomRotation(0.2),
-])
-if "ResNet" in config["project"]:
-    preprocess_input = tf.keras.applications.resnet_v2.preprocess_input
-    base_model = tf.keras.applications.resnet_v2.ResNet50V2(include_top=config["include_top"], weights=config["weights"])
-elif "VGG19" in config["project"]:
-    preprocess_input = tf.keras.applications.vgg19.preprocess_input
-    base_model = tf.keras.applications.vgg19.VGG19(include_top=config["include_top"], weights=config["weights"])
-global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-prediction_layer = tf.keras.layers.Dense(config["n_category"], activation='relu')
+        #%% create model
+        # declare layers
+        data_augmentation = tf.keras.Sequential([
+          tf.keras.layers.RandomFlip('horizontal'),
+          tf.keras.layers.RandomRotation(0.2),
+        ])
+        if "ResNet" in config["project"]:
+            preprocess_input = tf.keras.applications.resnet_v2.preprocess_input
+            base_model = tf.keras.applications.resnet_v2.ResNet50V2(include_top=config["include_top"], weights=config["weights"])
+        elif "VGG19" in config["project"]:
+            preprocess_input = tf.keras.applications.vgg19.preprocess_input
+            base_model = tf.keras.applications.vgg19.VGG19(include_top=config["include_top"], weights=config["weights"])
+        global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+        prediction_layer = tf.keras.layers.Dense(config["n_category"], activation='relu')
 
-# apply transfer learning to base model
-inputs = tf.keras.Input(shape=config["input_shape"])
-x = data_augmentation(inputs)
-x = preprocess_input(x)
-x = base_model(x, training=False)
-x = global_average_layer(x)
-outputs = prediction_layer(x)
-model = tf.keras.Model(inputs, outputs)
+        # apply transfer learning to base model
+        inputs = tf.keras.Input(shape=config["input_shape"])
+        x = data_augmentation(inputs)
+        x = preprocess_input(x)
+        x = base_model(x, training=False)
+        x = global_average_layer(x)
+        outputs = prediction_layer(x)
+        model = tf.keras.Model(inputs, outputs)
 
-lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(config["base_learning_rate"],
-                                                             decay_steps=config["decay_steps"]*n_steps,
-                                                             decay_rate=config['decay_rate'],
-                                                             staircase=True)
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(config.lr,
+                                                                     decay_steps=config["decay_steps"]*n_steps,
+                                                                     decay_rate=config['decay_rate'],
+                                                                     staircase=True)
 
-model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule, momentum=config["momentum"]),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=["accuracy"])
-model.summary()
+        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule, momentum=config["momentum"]),
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=["accuracy"])
+        model.summary()
 
-#%% Train de model
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        #%% Train de model
+        log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-model.fit(train_dataset,
-          epochs=config["initial_epochs"],
-          batch_size=config["batch_size"],
-          class_weight=class_weight,
-          validation_data=val_dataset,
-          callbacks=[tensorboard_callback, WandbMetricsLogger()])
-callbacks = [WandbCallback()]
+        model.fit(train_dataset,
+                  epochs=config["initial_epochs"],
+                  batch_size=config["batch_size"],
+                  validation_data=val_dataset,
+                  callbacks=[tensorboard_callback, WandbMetricsLogger()])
+        callbacks = [WandbCallback()]
 
-save_path = os.path.join(config["directory"], "saved_model")
-if not os.path.isdir(save_path):
-    os.mkdir(save_path)
-run.save()  # nned to call this before being able to get the name
-model.save(os.path.join(save_path, config["extraction_model"] + "_" + wandb.run.name))
+        save_path = os.path.join(config["directory"], "saved_model")
+        if not os.path.isdir(save_path):
+            os.mkdir(save_path)
+        # run.save()  # need to call this before being able to get the name
+        # model.save(os.path.join(save_path, config["extraction_model"] + "_" + wandb.run.name))
+        model.save(os.path.join(save_path, config["extraction_model"] + "_" + run.name))
+
+
+wandb.agent(sweep_id, train)
--- projects/behavourial/06_morph_space_comparison.py
+++ projects/behavourial/06_morph_space_comparison.py
@@ -10,7 +10,8 @@ run: python -m projects.behavourial.06_morph_space_comparison
 """
 
 #%% define computer path
-computer = 'windows'
+# computer = 'windows'
+computer = 'mac'
 if 'windows' in computer:
     computer_path = 'D:/Dataset/MorphingSpace'
     computer_letter = 'w'
@@ -31,10 +32,123 @@ CNN_path = os.path.join(computer_path, 'output', condition)
 #%% load data
 behav_data = np.load(os.path.join(behavioural_path, "human_avatar_orig.npy"))
 behav_data = np.moveaxis(behav_data, 0, -1)
-NRE_pred = np.load(os.path.join(NRE_path, "prob_grid_{}.npy".format(norm_type)))
-pred_data = NRE_pred[..., 1:]
-CNN_pred = np.load(os.path.join(CNN_path, "prob_grid_{}_{}.npy".format(condition, CNN_model)))
+NRE_pred = np.load(os.path.join(NRE_path, f"prob_grid_{norm_type}.npy"))
+NRE_pred = NRE_pred[..., 1:]
+CNN_pred = np.load(os.path.join(CNN_path, f"prob_grid_{condition}_{CNN_model}.npy"))
+CNN_pred = CNN_pred[..., 1:]
 
 print("shape behav_data", np.shape(behav_data))
 print("shape NRE_pred", np.shape(NRE_pred))
 print("shape CNN_pred", np.shape(CNN_pred))
+data = np.array([behav_data, NRE_pred, CNN_pred])
+
+#%% compute category
+categories = []
+for i in range(3):
+    argmax = np.argmax(data[i], axis=2)
+    cat = np.zeros(np.shape(data[i]))
+    for m in range(5):
+        for n in range(5):
+            cat[m, n, argmax[m, n]] = 1
+    categories.append(cat)
+categories = np.array(categories)
+
+NRE_cat_count = np.array(categories[0] - categories[1])
+NRE_cat_count[NRE_cat_count < 0] = 0
+NRE_cat_count = np.sum(NRE_cat_count)
+print("[NRE] ratio of categorization difference", NRE_cat_count/25 * 100)
+
+CNN_cat_count = np.array(categories[0] - categories[2])
+CNN_cat_count[CNN_cat_count < 0] = 0
+CNN_cat_count = np.sum(CNN_cat_count)
+print("[CNN] ratio of categorization difference", CNN_cat_count/25 * 100)
+print()
+
+#%% compute KL-divergence
+def KL_divergence(p, q):
+    return np.sum(p * np.log(p / q))
+
+
+def compute_morph_space_KL_div(p, q):
+    dim_x = np.shape(p)[0]
+    dim_y = np.shape(p)[1]
+
+    divergences = np.zeros((dim_x, dim_y))
+    for x in range(dim_x):
+        for y in range(dim_y):
+            div = KL_divergence(p[x, y], q[x, y])
+            divergences[x, y] = div
+
+    return divergences
+
+
+NRE_div = compute_morph_space_KL_div(behav_data, NRE_pred)
+CNN_div = compute_morph_space_KL_div(behav_data, CNN_pred)
+print("[NRE] sum div", np.sum(NRE_div))
+print(f"[NRE] mean: {np.mean(NRE_div)}, variance: {np.std(NRE_div)}")
+print()
+print("[CNN] sum div", np.sum(CNN_div))
+print(f"[CNN] mean: {np.mean(CNN_div)}, variance: {np.std(CNN_div)}")
+divergences = np.array([NRE_div, CNN_div])
+
+#%% plot comparison
+titles = ["behaviour", "NRE", "CNN"]
+fig = plt.figure(figsize=(9, 9))
+outer = gridspec.GridSpec(3, 3, wspace=0.2, hspace=0.2)
+
+# plot raw outputs
+for i in range(3):
+    # set outer titles
+    ax = plt.Subplot(fig, outer[i])
+    ax.set_title(titles[i])
+    ax.axis('off')
+    fig.add_subplot(ax)
+
+    inner = gridspec.GridSpecFromSubplotSpec(2, 2,
+                    subplot_spec=outer[i], wspace=0.1, hspace=0.1)
+
+    for j in range(4):
+        ax = plt.Subplot(fig, inner[j])
+        normalized = np.array(data[i, :, :, j])
+        normalized /= np.amax(normalized)
+        ax.imshow(normalized, cmap='viridis', interpolation='bilinear', vmax=1)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        fig.add_subplot(ax)
+
+# plot categorization
+for i in range(3):
+    # set outer titles
+    ax = plt.Subplot(fig, outer[i])
+    ax.axis('off')
+    fig.add_subplot(ax)
+
+    inner = gridspec.GridSpecFromSubplotSpec(2, 2,
+                                             subplot_spec=outer[i + 3], wspace=0.1, hspace=0.1)
+
+    for j in range(4):
+        ax = plt.Subplot(fig, inner[j])
+        im = ax.imshow(categories[i, ..., j], cmap='viridis', vmax=1)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        fig.add_subplot(ax)
+
+    # if i == 2:
+    #     fig.colorbar(im, ax=ax)
+
+# plot div
+for i in range(2):
+    ax = plt.Subplot(fig, outer[i + 7])
+    ax.axis('off')
+    im = ax.imshow(divergences[i], cmap='viridis', interpolation='bilinear', vmax=1)
+
+    # if i == 1:
+    #     fig.colorbar(im, ax=ax)
+
+    fig.add_subplot(ax)
+
+# plot plot bar
+ax = plt.Subplot(fig, outer[6])
+fig.colorbar(im, ax=ax)
+
+plt.show()
