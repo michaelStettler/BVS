e89b8ccf1e642a147978cc9871946ec814a6dd84
mstettler <michael.stettler@cin.uni-tuebingen.de>
2023-03-29 22:52:49 +0200

    Added saving of loss history in optimizer
    # Conflicts:
    #	utils/NRE_optimization/NRE_optimizer.py

--- projects/loss_optimization/01_create_alpha_plot.py
+++ projects/loss_optimization/01_create_alpha_plot.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 run: python -m projects.loss_optimization.01_create_alpha_plot
 """
 
-save_path = r'C:\Users\Alex\Documents\Uni\NRE\icann_results'
+# save_path = r'C:\Users\Alex\Documents\Uni\NRE\icann_results'
+save_path = 'D:/Dataset/FERG_DB_256/loss_optimization'
 
 with open(os.path.join(save_path, 'train_alpha_accuracy'), 'rb') as f:
     train_accuracy = pickle.load(f)
@@ -15,7 +16,13 @@ with open(os.path.join(save_path, 'train_alpha_accuracy'), 'rb') as f:
 with open(os.path.join(save_path, 'test_alpha_accuracy'), 'rb') as f:
     test_accuracy = pickle.load(f)
 
-plt.plot((test_accuracy[0.06]))
+for alpha in test_accuracy.keys():
+    print(alpha, np.max(test_accuracy[alpha]))
+    plt.plot(test_accuracy[alpha], label=str(alpha))
+plt.title('Test set classification accuracy for different balancing parameters')
+plt.xlabel('Training epoch')
+plt.ylabel('Accuracy')
+plt.legend()
 plt.show()
 
 
