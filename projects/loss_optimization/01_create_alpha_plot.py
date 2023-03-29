d7acacdb8f7002d237b847c0c323b9fd620253e7
alappe <alexander.lappe@uni-tuebingen.de>
2023-03-29 08:34:31 +0200

    Initialized plot script

--- projects/loss_optimization/01_create_alpha_plot.py
+++ projects/loss_optimization/01_create_alpha_plot.py
@@ -1,16 +1,21 @@
 import os
+import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 run: python -m projects.loss_optimization.01_create_alpha_plot
 """
 
-save_path = 'D:/Dataset/FERG_DB_256/loss_optimization'
+save_path = r'C:\Users\Alex\Documents\Uni\NRE\icann_results'
 
-train_accuracy = np.load(os.path.join(save_path, 'train_alpha_accuracy.npy'))
-test_accuracy = np.load(os.path.join(save_path, 'test_alpha_accuracy.npy'))
+with open(os.path.join(save_path, 'train_alpha_accuracy'), 'rb') as f:
+    train_accuracy = pickle.load(f)
 
-print("shape train_accuracy", train_accuracy.shape)
-print("shape test_accuracy", test_accuracy.shape)
+with open(os.path.join(save_path, 'test_alpha_accuracy'), 'rb') as f:
+    test_accuracy = pickle.load(f)
+
+plt.plot((test_accuracy[0.06]))
+plt.show()
 
 
